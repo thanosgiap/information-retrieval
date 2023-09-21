69a20ada649e7cf739a075ef3aa6934e3e42216c
thanosgiap <thanosgiap@gmail.com>
2023-09-21 19:25:21 +0300

    Setup Vite and SASS

--- develop/resize_file.php
+++ develop/resize_file.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 $inputCsvFile = 'develop\Greek_Parliament_Proceedings_1989_2020.csv';
 $outputCsvFile = 'develop\resized_proceedings.csv';
@@ -13,26 +13,25 @@ if (($inputHandle = fopen($inputCsvFile, 'r')) !== false) {
         for ($i = 0; $i < $numRowsToExtract; $i++) {
             // Read a row from the input CSV file
             $row = fgetcsv($inputHandle);
-            
+
             // If the row is false, we've reached the end of the input file
             if ($row === false) {
                 break;
             }
-            
+
             // Check if the last column is empty (assuming the last column is at index count($row) - 1)
             if (!empty($row[count($row) - 1]) && !empty($row[0])) {
                 // Write the row to the output CSV file
                 fputcsv($outputHandle, $row);
             }
         }
-        
+
         // Close the output CSV file
         fclose($outputHandle);
-       
     } else {
         echo "Error: Unable to open the output CSV file for writing\n";
     }
-    
+
     // Close the input CSV file
     fclose($inputHandle);
 } else {
